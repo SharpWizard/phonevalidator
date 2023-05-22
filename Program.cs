e760bd02af22f19fe1a96efa86bdfaa604a807d0
Tanvir Islam <35993901+itanvir546@users.noreply.github.com>
2023-05-22 23:56:43 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace phonevalidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Replace 'xxxxxxxxxx' with the actual phone number and 'xxx@x.com' with the actual email address
+            string phoneNumber = "xxxxxxxxxx";
+            string emailAddress = "xxx@mail.com";
+
+            // Construct the API endpoint URL with the provided phone number and email
+            string apiUrl = $"https://phonevalidator.xyz/api/validator.aspx/validate?phone={phoneNumber}&email={emailAddress}";
+
+            // Create a new WebClient instance
+            WebClient client = new WebClient();
+
+            try
+            {
+                // Make the HTTP GET request to the API
+                string response = client.DownloadString(apiUrl);
+
+                // Process the API response as needed
+                Console.WriteLine(response);
+            }
+            catch (WebException ex)
+            {
+                // An error occurred while making the request
+                Console.WriteLine("Error occurred: " + ex.Message);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
